36470b28f195034525e2ab75845d1a8e08fd8e0c
ronymc30 <ronymc30@yahoo.com>
2017-01-24 20:11:46 -0600

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ gap <- subData$Global_active_power
 plot(dateNtime, gap, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)", lwd = 1)
 
 #plot topright - Voltage
-plot(dateNtime, subData$Voltage, type = "l", xlab = "", ylab = "Voltage", lwd = 1)
+plot(dateNtime, subData$Voltage, type = "l", xlab = "datetime", ylab = "Voltage", lwd = 1)
 
 #plot bottomleft - Energy sub metering
 smeter1 <- subData$Sub_metering_1
@@ -26,10 +26,10 @@ smeter3 <- subData$Sub_metering_3
 plot(dateNtime, smeter1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(dateNtime, smeter2, type = "l", col = "red")
 lines(dateNtime, smeter3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = 1, lwd = 1, col = c("black","red","blue"))
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = 1, lwd = 1, bty = "n", col = c("black","red","blue"))
 
 #plot bottomright - Global Reactive Power
-plot(dateNtime, subData$Global_reactive_power, type = "l", xlab = "", ylab = "Global Reactive Power", lwd = 1)
+plot(dateNtime, subData$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power", lwd = 1)
 
 #close png, deallocate memory
 dev.off()
